281db76dfcbeafd567eb87c88e63fae1e9c3e1f5
Soham <sohampatwari144@gmail.com>
2025-02-04 17:56:18 +0530

    ^X /q

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,8 +1,37 @@
+
+import HeaderBox from '@/components/HeaderBox'
+import RightSidebar from '@/components/RightSidebar';
+import TotalBalanceBox from '@/components/TotalBalanceBox';
 import React from 'react'
 
 const Home = () => {
+
+  const loggendIn = {firstName : 'soham', lastName : 'Patwari', email : 'sohampatwari144@gmail.com'};
   return (
-    <div>Home</div>
+    <section className="home">
+      <div className="home-content">
+        <header className="home-header">
+          <HeaderBox
+            type="greeting"
+            title="Welcome"
+            user={loggendIn?.firstName || 'Guest'}
+            subtext="Access and manage your account and transaction efficiently"
+           />
+
+           <TotalBalanceBox 
+              accounts = {[]}
+              totalBanks = {1}
+              totalCurrentBalance={1250.35}
+           />
+        </header>
+      </div>
+
+      <RightSidebar 
+          user = {loggendIn}
+          transactions = {[]} 
+          banks = {[]}     
+      />
+    </section>
   )
 }
 
